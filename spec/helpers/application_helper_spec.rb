0ad2af23f33cb6468b875b3c059d7d4f816ff2ef
Pierre Sarnow <psarnow@gmail.com>
2012-05-19 05:23:18 -0600

    incuded exercises

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,15 +1,18 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the ApplicationHelper. For example:
-#
-# describe ApplicationHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       helper.concat_strings("this","that").should == "this that"
-#     end
-#   end
-# end
 describe ApplicationHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "full_title" do
+    it "should include the page title" do
+      full_title("foo").should =~ /foo/
+    end
+
+    it "should include the base title" do
+      full_title("foo").should =~ /^Ruby on Rails Tutorial Sample App/
+    end
+
+    it "should not include a bar for the home page" do
+      full_title("").should_not =~ /\|/
+    end
+  end
 end
